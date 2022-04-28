ce00d166e59db53017ba9787a829844a7f630341
Laudier2 <trabalhandooline2019@hotmail.com>
2022-04-28 00:00:56 -0300

    make it better

--- src/modules/db.js
+++ src/modules/db.js
@@ -6,16 +6,16 @@ const mongoose = require('mongoose')
  */
 
 //mongoose.connect("mongodb://localhost:27017/Users")
-mongoose.connect(process.env.URL_DB)
+mongoose.connect("mongodb+srv://kali:123hk@cluster0.nsdxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
-/**
- * E abaixo estamos utilizando o .then para acibir umamensage no console caso esteja fucionando corretamente 
- * e o .catch caso aconteca algum erro para mostra qual erro foi.
- */
+  /**
+   * E abaixo estamos utilizando o .then para acibir umamensage no console caso esteja fucionando corretamente 
+   * e o .catch caso aconteca algum erro para mostra qual erro foi.
+   */
 
-.then(() => {
-  console.log('A conecção com o mango foi bem sucedida OK!')
-})
-.catch((err) => {
-  console.log('Houve um erro ao se conectar com o mongo '+err)
-})
+  .then(() => {
+    console.log('A conecção com o mango foi bem sucedida OK!')
+  })
+  .catch((err) => {
+    console.log('Houve um erro ao se conectar com o mongo ' + err)
+  })
